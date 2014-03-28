89fa8097b343508edb65d11395165c3c37aab1f8
Julia <prudnikova@itcwin.com>
2014-03-28 17:40:21 +0400

    add C++ Class library

---
+++ OpticalMapping/OpticalMappingLib/OpticalMappingLib.h
@@ -0,0 +1,13 @@
+// OpticalMappingLib.h
+
+#pragma once
+
+using namespace System;
+
+namespace OpticalMappingLib {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ OpticalMapping/OpticalMappingLib/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// OpticalMappingLib.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
